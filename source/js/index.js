f1e95fde5b9801b6c2da55e9e4d2a68c47cfed57
zWalkerz <noah.vitalix@icloud.com>
2022-05-12 14:15:06 +0000

    ops

--- source/js/index.js
+++ source/js/index.js
@@ -207,7 +207,7 @@ function sharePlaylist(){
 function checkTag(e) {
 
     let errPattern = /.*?(#\w+)|.+/gm;
-    e = e.match();
+    e = e.match(errPattern);
     console.log(e)
 
     if(e.length > 0){
