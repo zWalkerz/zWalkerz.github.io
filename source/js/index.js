4eb8038d2a91b026122f674f47fd1de63b9de7cb
zWalkerz <noah.vitalix@icloud.com>
2022-08-04 18:46:15 +0200

    Magic

--- source/js/index.js
+++ source/js/index.js
@@ -99,6 +99,7 @@ var count = 0;
             section.appendChild(block);
             let menu = document.createElement("div");
             menu.setAttribute("id", "viewPlaylist" + count);
+            menu.setAttribute("class", "collapse");
             section.appendChild(menu);
             updated.push(el);
         })
@@ -536,11 +537,14 @@ function globalShared() {
         e.shared.forEach(el => {
 
             if (!updated.some(ell => JSON.stringify(ell) == JSON.stringify(el))) {
-                console.log(updated.some(ell => JSON.stringify(ell) == JSON.stringify(el)))
-                block = document.createElement("div");
+                let block = document.createElement("div");
                 block.setAttribute("class", "track");
-                block.innerHTML = "<div class='track__title'>" + el.name + "</div> <input type='text' class='label' value='" + el.desc + "' readonly spellcheck='false'><input type='text' class='label' value='" + el.tag.join() + "' readonly spellcheck='false'> <div class='controls'> <button onclick='addShared(this);' class='btn btn-outline-success' type='submit'>Add</button><button onclick='viewShared(this);' class='btn btn-outline-success' type='submit' data-bs-toggle='collapse' data-bs-target = '#viewPlaylist" + count + "'>View</button></div><div id='viewPlaylist"+ count+1 +"'> </div>";
+                block.innerHTML = "<div class='track__title'>" + el.name + "</div> <input type='text' class='label' value='" + el.desc + "' readonly spellcheck='false'><input type='text' class='label' value='" + el.tag.join() + "' readonly spellcheck='false'> <div class='controls'> <button onclick='addShared(this);' class='btn btn-outline-success' type='submit'>Add</button><button onclick='viewShared(this);' class='btn btn-outline-success' type='submit' data-bs-toggle='collapse' data-bs-target = '#viewPlaylist" + count + "'>View</button></div>";
                 section.appendChild(block);
+                let menu = document.createElement("div");
+                menu.setAttribute("id", "viewPlaylist" + count);
+                menu.setAttribute("class", "collapse");
+                section.appendChild(menu);
                 updated.push(el)
 
             }
