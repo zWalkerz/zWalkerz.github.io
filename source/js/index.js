821cccc6e0359f652cd849dc57b2dcb4332f44e7
zWalkerz <noah.vitalix@icloud.com>
2022-08-27 15:58:48 +0000

    bug

--- source/js/index.js
+++ source/js/index.js
@@ -1005,11 +1005,9 @@ function editing() {
     else {
 
         title.innerHTML = "Current playlist: " + currentPlaylist.name;
-        let block = document.createElement("div");
-        block.setAttribute("class", "track");
         currentPlaylist.songs.forEach(e => {
 
-            block.innerHTML += "<div class='track__art'> <img src= " + e.art + "></div><div class='track__title'>" + e.name + "</div><div class='label track__release_date'><span>" + e.release_date + "</span></div><div class='label track__duration'><span>" + e.duration + "</span> </div></div> <a href = '#'> <i class='bi bi-dash-lg' onclick='removeSong(this)' ></i></a>";
+            block.innerHTML += "<div class = 'track' ><div class='track__art'> <img src= " + e.art + "></div><div class='track__title'>" + e.name + "</div><div class='label track__release_date'><span>" + e.release_date + "</span></div><div class='label track__duration'><span>" + e.duration + "</span> </div></div> <a href = '#'> <i class='bi bi-dash-lg' onclick='removeSong(this)' ></i></a></div>";
         }
 
         );
